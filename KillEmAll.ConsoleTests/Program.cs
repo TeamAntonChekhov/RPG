c02db3a3b0ad4b43158433da2e4f4c21e513bdcf
Kalin Dimchev <kalin.dimchev@gmail.com>
2014-02-25 00:41:18 +0200

    Separate project for console tests added.

---
+++ KillEmAll.ConsoleTests/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KillEmAll.Common;
+
+namespace KillEmAll.ConsoleTests
+{
+    public class Program
+    {
+        private static Player GeneratePlayer()
+        {
+            return new Player("James Hetfield")
+            {
+                Health = 100,
+                AttackSkill = 6,
+                DefenseSkill = 5
+            };
+        }
+
+        private static Enemy GenerateEnemy()
+        {
+            return new Enemy("Justin")
+            {
+                Health = 80,
+                AttackSkill = 4,
+                DefenseSkill = 2
+            };
+        }
+
+        private static void TestVerySimpleBattleManager()
+        {
+            IBattleManager man = new VerySimpleBattleManager();
+
+            //  james strikes first
+            Player james = GeneratePlayer();
+            Enemy justin = GenerateEnemy();
+
+            Console.WriteLine(james);
+            Console.WriteLine(justin);
+
+            man.ResolveBattle(james, justin);
+
+            Console.WriteLine(james);
+            Console.WriteLine(justin);
+
+            //  justin strikes first
+            james = GeneratePlayer();
+            justin = GenerateEnemy();
+
+            Console.WriteLine(james);
+            Console.WriteLine(justin);
+
+            man.ResolveBattle(justin, james);
+
+            Console.WriteLine(james);
+            Console.WriteLine(justin);
+
+        }
+
+        static void Main(string[] args)
+        {
+            TestVerySimpleBattleManager();
+
+            Console.ReadLine();
+        }
+    }
+}
