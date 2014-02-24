588b13e51e838d3aa014cc4947833df8eeed359d
Kalin Dimchev <kalin.dimchev@gmail.com>
2014-02-25 01:04:21 +0200

    Some basic calculation of experience points gained by a player by killing an enemy.

--- KillEmAll.Common/Enemy.cs
+++ KillEmAll.Common/Enemy.cs
@@ -7,19 +7,46 @@ namespace KillEmAll.Common
 {
     public class Enemy : Character, IFighter
     {
+        private double health;
+        private double InitialHealth;
+
         public Enemy(string name)
             : base(name)
         {
 
         }
 
-        public double Health { get; set; }
+        public double Health {
+            get
+            {
+                return this.health;
+            }
+
+            set
+            {
+                //  initialize the initial health on the first assignment
+                //  we need it to include it into the Experience calculation
+                if (InitialHealth <= 0)
+                {
+                    this.InitialHealth = value;
+                }
+
+                this.health = value;
+            }
+        }
+        
+        
         public double AttackSkill { get; set; }
         public double DefenseSkill { get; set; }
 
+        public double ExperiencePointsGain()
+        {
+            return (AttackSkill + DefenseSkill)/2 + InitialHealth / 20;
+        }
+
         public override string ToString()
         {
-            return string.Format("{0}, Health: {1}, AttackSkill: {2}, DefenseSkill: {3}", base.ToString(), Health, AttackSkill, DefenseSkill);
+            return string.Format("{0}, Health: {1}, AttackSkill: {2}, DefenseSkill: {3}, ExpGain: {4}", base.ToString(), Health, AttackSkill, DefenseSkill, ExperiencePointsGain());
         }
     }
 }
